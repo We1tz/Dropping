e131dc491861ba4ae7e32339827b56f0bded3113
Weitz <73654789+We1tz@users.noreply.github.com>
2024-07-03 17:46:10 +0300

    upd

--- bot/top_information.py
+++ bot/top_information.py
@@ -5,10 +5,7 @@ def top():
     data = select_from_base()
     uinf = []
     for user in data:
-        name = user[1]
-        result = user[3]
-        count_tests = user[4]
-        uinf.append([name, int(result), int(count_tests)])
+        uinf.append([user[1], int(user[3]), int(user[4])])
     sorted_data = sorted(uinf, key=lambda x: x[1], reverse=True)
     return sorted_data
 
