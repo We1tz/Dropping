93de336ed359be052b1242c3c6b725836f0b704a
Weitz <73654789+We1tz@users.noreply.github.com>
2024-07-04 17:34:47 +0300

    upd - add api func

--- run_api.py
+++ run_api.py
@@ -2,12 +2,15 @@ import uvicorn
 import signal
 import sys
 
+
 def run_server():
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+
 
 def signal_handler(sig, frame):
     print("Server is shutting down...")
     sys.exit(0)
+
 
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal_handler)
