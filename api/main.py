ac5a8a03b1c5f67ebe650687f96eb684b9a2f8ef
Weitz <73654789+We1tz@users.noreply.github.com>
2024-07-05 16:31:23 +0300

    upd

--- api/main.py
+++ api/main.py
@@ -88,7 +88,7 @@ async def register(user_credentials: UserCredentials, response: Response):
     else:
         return {"result": add_result}
 
-#
+
 @app.get("/refresh")
 async def protected_route(request: Request):
     refresh_token = request.cookies.get("refresh_token")
