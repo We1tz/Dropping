a7e02d0e2fcaa67be040e78a925bec745ba17862
Weitz <73654789+We1tz@users.noreply.github.com>
2024-07-12 14:03:47 +0300

    Delete test_backend directory

--- test_backend/router/victorineRouter.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express').Router;
-const VictorineController = require('../controllers/victorine-controller')
-const Vicrouter = new Router();
-const authMiddleware = require('../middlewares/auth-middleware');
-
-Vicrouter.post('/post', authMiddleware, VictorineController.Getresses);
-Vicrouter.get('/get', authMiddleware, VictorineController.Givereses);
-
-module.exports = Vicrouter
