32d0053a2a5ac9450a1a9844cc98244c9f028273
we1tz <xarinivan2015@ya.ru>
2024-07-16 16:23:35 +0300

    update
    add model func

--- api/vacansy_model/test.py
+++ api/vacansy_model/test.py
@@ -1,3 +1,23 @@
-import nltk
-nltk.download('stopwords')
-ru_stopwords = stopwords.words("russian")
+import joblib
+from vacansy_pipline import VacansyPipline
+
+model = joblib.load('vacancy_model.pkl')
+
+
+pipline = VacansyPipline()
+
+
+def check_text_vacancy(text):
+    x = pipline.preprocess_text(text=text)
+    return model.predict(x)
+
+
+def check_url_vacancy(url):
+    x = pipline.preprocess_html(url)
+    return model.predict(x)
+
+
+def check_photo_vacancy(photo_path):
+    x = pipline.preprocess_img(photo_path)
+    return model.predict(x)
+
