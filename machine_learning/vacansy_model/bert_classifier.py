e683447a97a8a68c20d29e598e9c57f9feca91a1
IznslimedaD7 <iznslimeda@gmail.com>
2024-07-17 10:34:54 +0300

    Add predict_proba to model.

--- machine_learning/vacansy_model/bert_classifier.py
+++ machine_learning/vacansy_model/bert_classifier.py
@@ -140,3 +140,33 @@ class BertClassifier:
         prediction = torch.argmax(outputs.logits, dim=1).cpu().numpy()[0]
 
         return prediction
+
+    def predict_proba(self, text):
+        encoding = self.tokenizer.encode_plus(
+            text,
+            add_special_tokens=True,
+            max_length=self.max_len,
+            return_token_type_ids=False,
+            truncation=True,
+            padding='max_length',
+            return_attention_mask=True,
+            return_tensors='pt',
+        )
+        
+        out = {
+              'text': text,
+              'input_ids': encoding['input_ids'].flatten(),
+              'attention_mask': encoding['attention_mask'].flatten()
+          }
+        
+        input_ids = out["input_ids"].to(self.device)
+        attention_mask = out["attention_mask"].to(self.device)
+        
+        outputs = self.model(
+            input_ids=input_ids.unsqueeze(0),
+            attention_mask=attention_mask.unsqueeze(0)
+        )
+        lgSm = torch.nn.LogSoftmax(dim=1)
+        prediction = torch.exp(lgSm(outputs.logits)).cpu().detach().numpy()
+
+        return prediction
