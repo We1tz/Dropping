e0fd6e7c24199e66646556d28b5dd21cc204966e
IznslimedaD7 <iznslimeda@gmail.com>
2024-07-20 12:00:39 +0300

    Add analytic function which find feature transation which above mean.

--- machine_learning/transaction_model/analytical_function.py
+++ machine_learning/transaction_model/analytical_function.py
@@ -9,3 +9,20 @@ def mean_drop_features(df: pd.DataFrame, target_column_name='flag', date_column_
     d.pop(date_column_name)
     d.pop(target_column_name)
     return d
+
+
+def above_mean(mean_features_dict: dict[str, float], transation: pd.Series, target_column_name='flag', date_column_name='date',
+               ids_columns=['id_acc_out', 'id_acc_in']) -> dict:
+    drop_columns_name = ids_columns
+    drop_columns_name.append(target_column_name)
+    drop_columns_name.append(date_column_name)
+    transation = transation.drop(drop_columns_name)
+    suspect_matrix = []
+    features_name = transation.index.tolist()
+    for mean_feature, transation_feature in zip(list(mean_features_dict.values()), transation.tolist()):
+        if transation_feature > mean_feature:
+            suspect_matrix.append(1)
+        else:
+            suspect_matrix.append(0)
+    return dict(zip(features_name, suspect_matrix))
+    
